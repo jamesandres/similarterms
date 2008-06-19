3bebc23921301ec53b33029ac70ae6064ec8f02e
Robert Middleswarth <rmiddle@192183.no-reply.drupal.org>
2008-06-19 17:45:01 +0000

    inital 6 port commited to head

---
+++ similarterms.tpl.php
@@ -0,0 +1,21 @@
+<?php
+// $Id$
+
+/**
+ * @file simterms.tpl.php
+ *
+ * Theme implementation to display a list of related content.
+ *
+ * Available variables:
+ * - $items: the list.
+ */
+if ($items) {
+?>
+       <ul>
+       <?php foreach ($items as $node) { ?>
+               <li><?php print l($node->title, 'node/'. $node->nid) ?></li>
+       <?php } ?>
+       </ul>
+<?php
+}
+?>
