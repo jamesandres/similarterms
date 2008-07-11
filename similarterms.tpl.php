88cc72dba5a98ab54447238d90ced8fdf19a0af9
Robert Middleswarth <rmiddle@192183.no-reply.drupal.org>
2008-07-11 02:03:19 +0000

    [#180582] Added the option to display teasers.

--- similarterms.tpl.php
+++ similarterms.tpl.php
@@ -7,15 +7,24 @@
  * Theme implementation to display a list of related content.
  *
  * Available variables:
+ * - $teaser: Yes = 1. No = 0
  * - $items: the list.
  */
 if ($items) {
 $items_ls = array();
+  if (!$teaser) {
+    foreach ($items as $node) {
+      print l($node->title, 'node/'. $node->nid);
+      print ' - '. $node->teaser;
+    }
+  }
+  else {
 ?>
-       <?php foreach ($items as $node) { ?>
+     <?php foreach ($items as $node) { ?>
                <?php $items_ls[] = l($node->title, 'node/'. $node->nid);  ?>
        <?php } ?>
        <?php print theme('item_list', $items_ls); ?>
 <?php
+  }
 }
 
