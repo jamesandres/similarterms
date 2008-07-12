dc8b986571fc201b1894bb9c94007ccc5291991e
Robert Middleswarth <rmiddle@192183.no-reply.drupal.org>
2008-07-12 23:18:55 +0000

    [#281804] Fix for new teaser code being backwards.

--- similarterms.tpl.php
+++ similarterms.tpl.php
@@ -7,22 +7,24 @@
  * Theme implementation to display a list of related content.
  *
  * Available variables:
- * - $teaser: Yes = 1. No = 0
+ * - $display_options:
+ *    'title_only' => 'Display titles only',
+ *    'teaser' => 'Display titles and teaser',
  * - $items: the list.
  */
 if ($items) {
 $items_ls = array();
-  if (!$teaser) {
+  if ($display_options == 'title_only') {
+    foreach ($items as $node) {
+      $items_ls[] = l($node->title, 'node/'. $node->nid);
+    }
+    print theme('item_list', $items_ls);
+  }
+  if ($display_options == 'teaser') {
     foreach ($items as $node) {
       print '<li>'. l($node->title, 'node/'. $node->nid);
       print ' - '. $node->teaser;
       print "</li>\n";
     }
-  }
-  else {
-    foreach ($items as $node) {
-      $items_ls[] = l($node->title, 'node/'. $node->nid);
-    }
-    print theme('item_list', $items_ls);
   }
 }
