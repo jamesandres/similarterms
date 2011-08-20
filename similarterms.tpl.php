ebd721a746428c68341591f39e2261299c1f029d
Robert Middleswarth <robert@middleswarth.net>
2011-08-20 04:38:39 -0400

    Cleaned up the code.  Now works the same as 6.x-1.x branch did.  Although I added an option to display an empty block.

--- similarterms.tpl.php
+++ similarterms.tpl.php
@@ -9,8 +9,10 @@
  * - $display_options:
  *    'title_only' Display titles only
  *    'teaser'     Display titles and teaser
+ * - $display_block: If it should return an empy list or nothing at all.
  * - $items: the list.
  * - $nodes: The raw data of the listed items.
  */
-?>
-<?php print($items); ?>
+if ($display_block) {
+  print($items);
+}
