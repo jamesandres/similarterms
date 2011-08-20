8879f776ecf6694a10936db27c46d01eda46fc84
sutharsan <sutharsan@73854.no-reply.drupal.org>
2011-08-20 02:22:05 -0400

    Issue #1045378 by Sutharsan: Undefined property: stdClass::$multiple in similarterms_block_info() .

--- similarterms.tpl.php
+++ similarterms.tpl.php
@@ -7,27 +7,10 @@
  *
  * Available variables:
  * - $display_options:
- *    'title_only' => 'Display titles only',
- *    'teaser' => 'Display titles and teaser',
+ *    'title_only' Display titles only
+ *    'teaser'     Display titles and teaser
  * - $items: the list.
+ * - $nodes: The raw data of the listed items.
  */
-if ($items) {
-$items_ls = array();
-  if ($display_options == 'title_only') {
-    foreach ($items as $node) {
-      $items_ls[] = l($node->title, 'node/'. $node->nid);
-    }
-    print theme('item_list', $items_ls);
-  }
-  if ($display_options == 'teaser') {
-    $output = '';
-    foreach ($items as $node) {
-      $output .= '<li>'. l($node->title, 'node/'. $node->nid);
-      $output .= ' - '. $node->teaser;
-      $output .= "</li>\n";
-    }
-    if ($output) {
-      echo "<ul>" . $output . "</ul>";
-    }
-  }
-}
+?>
+<?php print($items); ?>
